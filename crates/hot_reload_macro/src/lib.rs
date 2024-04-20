cb13c0341c6bf9b50e510c2851a22bcce5dd1da0
green-ogre <109698029+green-ogre@users.noreply.github.com>
2024-04-19 19:24:04 -0600

    texture 2d

--- crates/hot_reload_macro/src/lib.rs
+++ crates/hot_reload_macro/src/lib.rs
@@ -7,59 +7,63 @@ pub fn hot_reload(
     _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let f = parse_macro_input!(item as syn::ItemFn);
-    let name = &f.sig.ident;
-    let internal_name = format_ident!("__internal_{}", name);
-    let internal_name_as_str = &internal_name.to_string();
-    let args = &f.sig.inputs;
-    let arg_idents: Vec<Ident> = f
-        .sig
-        .inputs
-        .pairs()
-        .map(|p| match p.value() {
-            syn::FnArg::Receiver(_) => panic!(),
-            syn::FnArg::Typed(t) => match *t.pat.clone() {
-                syn::Pat::Ident(i) => i.ident.clone(),
-                _ => panic!(),
-            },
-        })
-        .collect();
-    let arg_types: Vec<_> = f
-        .sig
-        .inputs
-        .pairs()
-        .map(|p| match p.value() {
-            syn::FnArg::Receiver(_) => panic!(),
-            syn::FnArg::Typed(t) => *t.ty.clone(),
-        })
-        .collect();
-    let body = &f.block;
+    if cfg!(windows) {
+        let f = parse_macro_input!(item as syn::ItemFn);
+        let name = &f.sig.ident;
+        let internal_name = format_ident!("__internal_{}", name);
+        let internal_name_as_str = &internal_name.to_string();
+        let args = &f.sig.inputs;
+        let arg_idents: Vec<Ident> = f
+            .sig
+            .inputs
+            .pairs()
+            .map(|p| match p.value() {
+                syn::FnArg::Receiver(_) => panic!(),
+                syn::FnArg::Typed(t) => match *t.pat.clone() {
+                    syn::Pat::Ident(i) => i.ident.clone(),
+                    _ => panic!(),
+                },
+            })
+            .collect();
+        let arg_types: Vec<_> = f
+            .sig
+            .inputs
+            .pairs()
+            .map(|p| match p.value() {
+                syn::FnArg::Receiver(_) => panic!(),
+                syn::FnArg::Typed(t) => *t.ty.clone(),
+            })
+            .collect();
+        let body = &f.block;
 
-    let reloaded_f = quote! {
-        #[no_mangle]
-        pub fn #internal_name (#args) {
-            #body
+        let reloaded_f = quote! {
+            #[no_mangle]
+            pub fn #internal_name (#args) {
+                #body
+            }
+        };
+
+        let loader_f = quote! {
+            use ::winny::prelude::*;
+            pub fn #name (#args __internal_lib: Res<::winny::hot_reload::LinkedLib>) {
+                let f = unsafe {
+                    __internal_lib
+                        .linked_lib
+                        .lib
+                        .as_ref()
+                        .unwrap()
+                        .get::<fn(#(#arg_types,)*)>(#internal_name_as_str.as_bytes()).unwrap()
+                };
+                f(#(#arg_idents,)*);
+            }
+        };
+
+        quote! {
+            #reloaded_f
+            #loader_f
         }
-    };
-
-    let loader_f = quote! {
-        use ::winny::prelude::*;
-        pub fn #name (#args __internal_lib: Res<::winny::hot_reload::LinkedLib>) {
-            let f = unsafe {
-                __internal_lib
-                    .linked_lib
-                    .lib
-                    .as_ref()
-                    .unwrap()
-                    .get::<fn(#(#arg_types,)*)>(#internal_name_as_str.as_bytes()).unwrap()
-            };
-            f(#(#arg_idents,)*);
-        }
-    };
-
-    quote! {
-        #reloaded_f
-        #loader_f
+        .into()
+    } else {
+        item
     }
-    .into()
 }
--- crates/winny_engine/src/gfx/shader2.wgsl
+++ crates/winny_engine/src/gfx/shader2.wgsl
@@ -1,28 +1,29 @@
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,    
-    @location(0) tex_coords: vec2<f32> ,
+    @location(0) tex_coords: vec2<f32>,
 }
 
 @vertex
 fn vs_main(@builtin(vertex_index) index: u32) -> VertexOutput {
-    // var out: VertexOutput;
-    // var x: f32 = f32((index << 1) & 2);
-    // var y: f32 = f32(index & 2);
-    // out.tex_coords = vec2<f32>(x, y);
-    // out.clip_position = vec4<f32>(out.tex_coords * vec2<f32>(2, -2) + vec2<f32>(-1, 1), 0, 1);
-
-    // return out;
-
     var out: VertexOutput;
+
     let x = f32(index & 2) * 2.0 - 1.0;
     let y = f32(index & 1) * 4.0 - 1.0;
     out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
-    out.tex_coords = vec2<f32>((out.clip_position.x + 1.0) / 4.0, (out.clip_position.y + 1.0) / 4.0);
+
+    let tex_x = (out.clip_position.x + 1.0) / 2.0;
+    let tex_y = 1.0 - ((out.clip_position.y + 1.0) / 2.0);
+    out.tex_coords = vec2<f32>(tex_x, tex_y);
 
     return out;
 }
 
+@group(0) @binding(0)
+var t_diffuse: texture_2d<f32>;
+@group(0)@binding(1)
+var s_diffuse: sampler;
+
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.tex_coords, 1.0, 1.0);
+    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
 }
